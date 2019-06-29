1b16db333640e4506b862ef2c7e95d5f97b2b4ef
dron <dr_dronych@mail.ru>
2019-06-29 19:37:38 +0600

    bug fix: periodicity of the remainder function

--- fibonacci_remainder.py
+++ fibonacci_remainder.py
@@ -1,3 +1,4 @@
+# python3
 import sys
 
 
@@ -31,15 +32,43 @@ def fib_remainder(n, m):
         remainder from Fib(n) modulo m
     """
 
-    period_len = (m - 2) * 5 + 3
-    remainder = n % period_len
-    cache = [0, 1]
+    # period_len = (m - 2) * 5 + 3
+    cache, mod_cache = [0, 1], [0, 1]
 
-    for i in range(1, remainder + 1):
-        fib_i = cache[i - 1] + cache[i]
+    if n == 0 or n == 1:
+        return cache[0] if n == 0 else cache[1]
+
+    # determining periodicity of Fib(n) % m
+    i = 2
+    occurrence_count = 1
+
+    while occurrence_count <= 1:
+
+        fib_i = cache[i - 2] + cache[i - 1]
         cache.append(fib_i)
 
-    return cache[-2] % m
+        mod_cache.append(fib_i % m)
+
+        i += 1
+
+        if mod_cache[i - 2] == 0 and mod_cache[i - 1] == 1:
+            occurrence_count += 1
+
+    period_len = len(mod_cache[:-2])
+    remainder = n % period_len
+
+    # determining the actual remainder w/
+    # the help of calculated remainder
+    cache = [0, 1]
+
+    if remainder == 0 or remainder == 1:
+        return cache[0] if remainder == 0 else cache[1]
+
+    for i in range(2, remainder + 1):
+        fib_i = cache[i - 2] + cache[i - 1]
+        cache.append(fib_i)
+
+    return cache[-1] % m
 
 
 if __name__ == '__main__':
