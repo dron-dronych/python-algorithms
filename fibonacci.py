2fe8732b222262c6b047eeecc5eb33e4f081f5a9
dron <dr_dronych@mail.ru>
2019-07-18 17:09:51 +0600

    fixed a minor bug

--- fibonacci.py
+++ fibonacci.py
@@ -1,3 +1,5 @@
+# python3
+
 def fib_naive(n):
     assert n >= 0, 'n must be greater than 0!'
 
@@ -9,24 +11,25 @@ def fib_naive(n):
 
 
 def fib_fast(n):
-    assert n >= 0, 'n must be greater than 0!'
+    assert n >= 0, 'n can not be less than 0!'
+
+    if n == 0 or n == 1:
+        return 0 if n == 0 else 1
 
     sequence = []
     sequence.extend([0, 1])
 
     if n > 1:
-        for i in range(2,n):
+        for i in range(2, n + 1):
             sequence.append(sequence[i - 2] + sequence[i - 1])
 
-    return sequence[n - 1]
+    return sequence[-1]
 
 
 if __name__ == '__main__':
     n = int(input())
-    print('Fib of {0} is : {1}'.format(
-            n, fib_naive(n))
-    )
-
     # print('Fib of {0} is : {1}'.format(
-    #     n, fib_fast(n))
+    #         n, fib_naive(n))
     # )
+
+    print(fib_fast(n))
