2a1137947bd66dbd65227ec61d1ccb1a92eb69be
dron <dr_dronych@mail.ru>
2020-06-06 00:16:25 +0600

    fixed impl of car fueling algo

--- greedy/car_fueling.py
+++ greedy/car_fueling.py
@@ -13,17 +13,20 @@ def car_fueling(d, m, n, *args):
 
     # safe move -- refill at farthest stop possible to
     # travel w/out a refill
+    stops = [arg for arg in args]
+    stops.append(d)  # last point on the route
 
-
+    prev_refill = 0
     prev_stop = 0
 
-    for i,stop in enumerate(args):
-        if m > stop - prev_stop:
+    for i,stop in enumerate(stops):
+        if m > stop - prev_refill:
             best_stop = stop
+            prev_stop = stop
 
         else:
             num_refills += 1
-            prev_stop = stop
+            prev_refill = prev_stop
             distance_left -= stop
 
     if distance_left > m:
