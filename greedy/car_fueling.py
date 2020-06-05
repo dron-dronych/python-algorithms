f2b7f6fcb9df8561d4d6f622fe8e85f3122f25e0
dron <dr_dronych@mail.ru>
2020-06-06 02:01:33 +0600

    fixed a bug in car fueling problem & added docs

--- greedy/car_fueling.py
+++ greedy/car_fueling.py
@@ -1,27 +1,29 @@
-def car_fueling(d, m, n, *args):
+def car_fueling(distance, capacity, n_stations, *args):
     """
+    find min number of refills along travel distance
 
-    :param d:
-    :param m:
-    :param n:
-    :param args:
-    :return:
+    :param distance: int: distance to travel
+    :param capacity: int: full tank capacity
+    :param n_stations: int: num of gas stations along the way
+    :param args: gas stations at distance arg1, arg2,..., argn along the way
+    :return: int: min number of refills to travel distance
     """
 
     num_refills = 0
-    distance_left = d
+    distance_left = distance
 
     # safe move -- refill at farthest stop possible to
     # travel w/out a refill
     stops = [arg for arg in args]
-    stops.append(d)  # last point on the route
+    stops.append(distance)  # last point on the route
 
     prev_refill = 0
     prev_stop = 0
 
-    for i,stop in enumerate(stops):
-        if m > stop - prev_refill:
-            best_stop = stop
+    for stop in stops:
+        if capacity < stop - prev_stop:
+            return -1
+        if capacity > stop - prev_refill:
             prev_stop = stop
 
         else:
@@ -29,12 +31,11 @@ def car_fueling(d, m, n, *args):
             prev_refill = prev_stop
             distance_left -= stop
 
-    if distance_left > m:
-        return -1
-    else:
-        return num_refills
+    return num_refills
 
 
 if __name__ == '__main__':
     print(car_fueling(950, 400, 4, 200, 375, 550, 750))
+    print(car_fueling(10, 3, 4, 1, 2, 5, 9))
+    print(car_fueling(200, 250, 2, 100, 150))
 
