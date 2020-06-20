08f1c3814f9d6e601c300f72efca6e7a4cb5b274
dron <dr_dronych@mail.ru>
2020-06-20 23:24:56 +0600

    added problem description to car fueling problem

--- greedy/car_fueling.py
+++ greedy/car_fueling.py
@@ -1,3 +1,9 @@
+# You are going to travel to another city that is located 𝑑 miles away from your home city.
+# Your car can travel at most 𝑚 miles on a full tank and you start with a full tank. Along your way,
+# there are gas stations at distances stop1, stop2, . . . , stop𝑛 from your home city.
+# What is the minimum number of refills needed?
+
+
 def car_fueling(distance, capacity, n_stations, *args):
     """
     find min number of refills along travel distance
