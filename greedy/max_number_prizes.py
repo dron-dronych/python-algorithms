eb9374b2367c11c57ab8cb99e291c806d8a84d61
dron-dronych <dr_dronych@mail.ru>
2020-06-23 23:50:47 +0600

    max number of prizes - WIP

---
+++ greedy/max_number_prizes.py
@@ -0,0 +1,9 @@
+# The goal of this problem is to represent a given positive integer n as a sum of as many pairwise
+# distinct positive integers as possible. That is, to find the maximum k such that n can be written as
+# a 1 + a 2 + · · · + a k where a 1 , . . . , a k are positive integers and a i ̸ = a j for all 1 ≤ i < j ≤ k.
+
+
+# TODO implement
+def max_num_prizes(n_candies):
+    """"""
+    pass
