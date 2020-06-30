3e58d11de7396b42b5a1ed4c34feca8fd5e01e32
dron <dr_dronych@mail.ru>
2020-06-30 11:13:29 +0600

    added main func to test implementation

--- greedy/collecting_signatures.py
+++ greedy/collecting_signatures.py
@@ -16,9 +16,34 @@ def collect_signatures(n_segments, *args):
 
     points = set()
     num_points = 0
+    prev_start = 0
+    prev_end = 0
+    best_start = 0
+    best_end = 0
 
     for coord in args:
         split_coord = coord.split(' ')
         start, end = int(split_coord[0]), int(split_coord[1])
 
-        points.add(end)
+
+
+        points.add(best_point)
+
+
+
+        prev_end = end
+
+    return num_points, points
+
+
+if __name__ == '__main__':
+    print(
+        collect_signatures(3, '1 3', '2 5', '3 6')
+    )
+
+    print(
+        collect_signatures(4, '4 7', '1 3', '2 5', '5 6')
+    )
+
+
+
