aaf21e2df476916518fdd910b1d18149a32ed281
dron-dronych <dr_dronych@mail.ru>
2020-07-13 22:00:39 +0600

    impl of max salary algo

---
+++ greedy/max_salary.py
@@ -0,0 +1,44 @@
+def max_salary(n_ints, *args):
+    """
+
+    :param n_ints: int: num of arguments
+    :param args: integers
+    :return: max int comprised of *args
+    """
+    if n_ints < 1 or n_ints > 100:
+        raise ValueError('num of integers out of range [1, 100]!')
+
+    max_num = -1
+    max_num_id = -1
+    numbers = list(args)
+    res = []
+
+    while len(numbers) > 0:
+        for i,n in enumerate(numbers):
+            if n >= max_num:
+                max_num = n
+                max_num_id = i
+        res.append(max_num)
+        numbers.pop(max_num_id)
+
+    return res
+
+
+if __name__ == '__main__':
+    print(max_salary(2,
+                     21,
+                     2
+                     ))
+
+    print(max_salary(
+        5, 9, 4, 6, 1, 9
+    ))
+
+    print(max_salary(
+        3, 23, 39, 92
+    ))
+
+
+
+
+
