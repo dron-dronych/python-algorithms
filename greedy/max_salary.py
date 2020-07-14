ec470375d43e8b02df23b3837349e32b5223642b
dron-dronych <dr_dronych@mail.ru>
2020-07-14 17:11:31 +0600

    bug fix in max salary algo

--- greedy/max_salary.py
+++ greedy/max_salary.py
@@ -8,12 +8,13 @@ def max_salary(n_ints, *args):
     if n_ints < 1 or n_ints > 100:
         raise ValueError('num of integers out of range [1, 100]!')
 
-    max_num = -1
-    max_num_id = -1
+
     numbers = list(args)
     res = []
 
     while len(numbers) > 0:
+        max_num = -1
+        max_num_id = -1
         for i,n in enumerate(numbers):
             if n >= max_num:
                 max_num = n
@@ -25,9 +26,7 @@ def max_salary(n_ints, *args):
 
 
 if __name__ == '__main__':
-    print(max_salary(2,
-                     21,
-                     2
+    print(max_salary(2, 21, 2
                      ))
 
     print(max_salary(
