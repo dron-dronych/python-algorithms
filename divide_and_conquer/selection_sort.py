9f35ff886dfcc8c366664e3a9f76cc6e311c7b0c
dron-dronych <dr_dronych@mail.ru>
2020-12-01 21:12:22 +0600

    selection sort impl

---
+++ divide_and_conquer/selection_sort.py
@@ -0,0 +1,24 @@
+def selection_sort(arr):
+    """
+    selection sort O(n^2)
+    :param arr:
+    :return:
+    """
+    for i in range(len(arr)):
+        min_element = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_element]:
+                min_element = j
+
+        tmp = arr[min_element]
+        arr[i] = arr[min_element]
+        arr[min_element] = tmp
+
+    return arr
+
+
+if __name__ == '__main__':
+    arr = [12, 13, 43, 6, 890]
+    print(arr)
+
+    print(selection_sort(arr))
