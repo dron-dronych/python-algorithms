767976bd0a51033416714c8ef75289b68d8d7019
dron <dr_dronych@mail.ru>
2021-01-13 19:17:56 +0600

    single element binary search helper function test

--- divide_n_conquer/binary_search.py
+++ divide_n_conquer/binary_search.py
@@ -14,27 +14,47 @@ def binary_search(n, arr1, k, arr2):
 
      return [2 0 -1 0 -1]
     """
+    pass
 
 
 def _binary_search(arr, x):
     """
     find index of x in arr
     :param arr: list: sorted array of integers
-    :param x: int: ineteger to search
+    :param x: int: integer to search
     :return: index in arr or -1 if not found
     """
     n = len(arr)
     mid_index = n // 2
+    # print(mid_index)
+    pointer = n - 1
+    print(pointer)
+
 
     if n == 1 and arr[mid_index] != x:
         return -1
 
+    if n == 1 and arr[mid_index] == x:
+        # print(arr[mid_index])
+        return pointer
+
     left = arr[:mid_index]
     right = arr[mid_index:]
+    # print(left)
 
     if x <= left[-1]:
+        pointer -= len(right)
         pos = _binary_search(left, x)
+
     else:
+        pointer += len(left)
         pos = _binary_search(right, x)
 
     return pos
+
+
+if __name__ == '__main__':
+    arr = [0, 1, 3, 17, 40, 40]
+    elem = 3
+
+    print(_binary_search(arr, elem))
