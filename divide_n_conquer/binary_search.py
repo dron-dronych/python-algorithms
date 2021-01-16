5e7d0b8ca87075f7c69b67c1abf1a512560c80eb
dron <dr_dronych@mail.ru>
2021-01-16 20:16:15 +0600

    binary search helper debug & fix

--- divide_n_conquer/binary_search.py
+++ divide_n_conquer/binary_search.py
@@ -17,41 +17,52 @@ def binary_search(n, arr1, k, arr2):
     pass
 
 
-def _binary_search(arr, x, start=0, end=5):
+def _binary_search(arr, x, start, end):
     """
     find index of x in arr
     :param arr: list: sorted array of integers
     :param x: int: integer to search
     :return: index in arr or -1 if not found
     """
-    n = len(arr)
-    mid_index = n // 2
-    # print(mid_index_old)
+    n = end - start + 1
+    # mid_index = n // 2
+    # print(mid_index)
+    # print(start, end)
 
-
+    mid_index = int(start + (end - start) / 2)
+    # n = len(arr)
+    # print(n)
+    print(start, end)
 
     if n == 1 and arr[mid_index] != x:
         return -1
 
-    if n == 1 and arr[mid_index] == x:
+    # if start < end:
+    #     return start - 1
+
+    # if end < start:
+    #     return start - 1
+
+    if arr[mid_index] == x:
         # print(arr[mid_index])
         return mid_index
 
     left = arr[:mid_index]
     right = arr[mid_index:]
-    # print(left)
+    # print(left, right)
+    # print(mid_index)
 
-    if x <= left[-1]:
-        pos = _binary_search(left, x, end=mid_index)
+    if x < arr[mid_index]:
+        pos = _binary_search(arr, x, start, mid_index -1)
 
     else:
-        pos = _binary_search(right, x, start=mid_index)
+        pos = _binary_search(arr, x, mid_index + 1, end)
 
     return pos
 
 
 if __name__ == '__main__':
     arr = [0, 1, 3, 17, 40, 50]
-    elem = 40
+    elem = 3
 
-    print(_binary_search(arr, elem))
+    print(_binary_search(arr, elem, 0, 5))
