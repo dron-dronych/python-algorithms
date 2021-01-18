1a6fd8fd47fbd7ff001b19d681e92578bad81ac0
dron <dr_dronych@mail.ru>
2021-01-18 23:27:45 +0600

    binary search completion

--- divide_n_conquer/binary_search.py
+++ divide_n_conquer/binary_search.py
@@ -9,12 +9,17 @@ def binary_search(n, arr1, k, arr2):
 
     Example:
 
-     n = 5, arr1 = [5 1 5 8 12 13]
+     n = 5, arr1 = [1 5 8 12 13]
      k = 5, arr2 = [ 8 1 23 1 11]
 
      return [2 0 -1 0 -1]
     """
-    pass
+    res = []
+
+    for elem in arr2:
+        res.append(_binary_search(arr1, elem, 0, 4))
+        print(res)
+    return res
 
 
 def _binary_search(arr, x, start, end):
@@ -32,7 +37,7 @@ def _binary_search(arr, x, start, end):
     mid_index = int(start + (end - start) / 2)
     # n = len(arr)
     # print(n)
-    print(start, end)
+    # print(start, end)
 
     if n == 1 and arr[mid_index] != x:
         return -1
@@ -40,8 +45,6 @@ def _binary_search(arr, x, start, end):
     # if start < end:
     #     return start - 1
 
-    # if end < start:
-    #     return start - 1
 
     if arr[mid_index] == x:
         # print(arr[mid_index])
@@ -62,7 +65,7 @@ def _binary_search(arr, x, start, end):
 
 
 if __name__ == '__main__':
-    arr = [0, 1, 3, 17, 40, 50]
-    elem = 3
+    arr1 = [1, 5, 8, 12, 13]
+    arr2 = [8, 1, 23, 1, 11]
 
-    print(_binary_search(arr, elem, 0, 5))
+    print(binary_search(5, arr1, 5, arr2))
