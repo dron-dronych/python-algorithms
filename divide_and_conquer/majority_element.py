f638a3ec9ce4806fae2986f93a8ccea4636017f4
dron <dr_dronych@mail.ru>
2021-01-22 22:34:10 +0600

    majority element algorithm impl

--- divide_and_conquer/majority_element.py
+++ divide_and_conquer/majority_element.py
@@ -5,4 +5,15 @@ def majority_element(n, arr):
     :param arr: list: a sequence of 𝑛 non-negative integers 𝑎0, 𝑎1, . . . , 𝑎𝑛−1.
     :return: int: 1 or 0 if arr contains a majority element
     """
-    pass
+    counter = 0
+    mid = n // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    # base case
+    if left == right:
+        return 0
+    counter += 1
+
+    if counter > n / 2:
+        return 1
