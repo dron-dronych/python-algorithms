3796eace63458407bdf86b8a052a55e66dd01f70
dron <dr_dronych@mail.ru>
2021-01-23 20:15:04 +0600

    majority element algorithm impl - progress saver

--- divide_and_conquer/majority_element.py
+++ divide_and_conquer/majority_element.py
@@ -1,18 +1,22 @@
-def majority_element(n, arr):
+def majority_element(n, arr, start=0, end=5):
     """
     check whether an input sequence contains a majority element
     :param n: int: num of elements in arr
     :param arr: list: a sequence of 𝑛 non-negative integers 𝑎0, 𝑎1, . . . , 𝑎𝑛−1.
     :return: int: 1 or 0 if arr contains a majority element
     """
-    counter = 0
-    mid = n // 2
-    left = arr[:mid]
-    right = arr[mid:]
-
     # base case
+    if start == end:
+        return 1
+
+    counter = 0
+    mid = start + (end - start) // 2
+
+    left = majority_element(n, arr, end=mid)
+    right = majority_element(n, arr, start=mid + 1)
+
     if left == right:
-        return 0
+        return 1
     counter += 1
 
     if counter > n / 2:
