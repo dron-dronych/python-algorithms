bdc6c5bd830cae3b70b539765edfa0774b60727a
dron <dr_dronych@mail.ru>
2021-01-26 23:43:17 +0600

    three way quick sort starter

---
+++ divide_and_conquer/improve_quick_sort.py
@@ -0,0 +1,8 @@
+def three_way_quicksort(n, arr):
+    """
+    optimized quick sort for arrays with few unique elements
+    :param n: int: num of elements in arr
+    :param arr: list: array of elements to sort
+    :return: sorted: list: sorted array
+    """
+    pass
