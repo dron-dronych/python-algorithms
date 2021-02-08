9f8e6bc8d24e946d678b688b389a58400b7447a5
dron <dr_dronych@mail.ru>
2021-02-08 23:54:04 +0600

    lottery algo idea testing

--- divide_and_conquer/lottery.py
+++ divide_and_conquer/lottery.py
@@ -3,7 +3,20 @@ import sys
 
 def fast_count_segments(starts, ends, points):
     cnt = [0] * len(points)
-    #write your code here
+
+    # combine points and segments into one array
+    combined = []
+
+    starts_tuples = list(map(lambda x: (x, 'l'), starts))
+    ends_tuples = list(map(lambda x: (x, 'r'), ends))
+    points_tuples = list(map(lambda x: (x, 'p'), points))
+
+    combined.extend(starts_tuples)
+    combined.extend(ends_tuples)
+    combined.extend(points_tuples)
+
+
+
     return cnt
 
 def naive_count_segments(starts, ends, points):
@@ -15,8 +28,10 @@ def naive_count_segments(starts, ends, points):
     return cnt
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+    # input = sys.stdin.read()
+
+    # data = list(map(int, input.split()))
+    data = [3, 2, 0, 5, -3, 2, 7, 10, 1, 6]
     n = data[0]
     m = data[1]
     starts = data[2:2 * n + 2:2]
