34ded5a7ddd170a242458a42310c47d3a5c62eb7
dron-dronych <dr_dronych@mail.ru>
2021-08-31 23:25:52 +0200

    started minimum num of operations in primitive calculator

---
+++ dynamic_programming/calculator.py
@@ -0,0 +1,19 @@
+# Given an integer n, compute the minimum number of operations needed to obtain the number n
+# starting from the number 1.
+
+def primitive_calculator(n):
+    """
+
+    Parameters
+    ----------
+    n
+
+    Returns
+    -------
+
+    """
+    k = 1
+    intermediate_results = []
+
+
+    return k, intermediate_results
