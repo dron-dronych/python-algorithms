198359000b487c98537e96a748f3de6d13bd23c3
dron-dronych <dr_dronych@mail.ru>
2021-09-01 22:39:50 +0200

    primitive calculator progress saver

--- dynamic_programming/calculator.py
+++ dynamic_programming/calculator.py
@@ -1,6 +1,8 @@
 # Given an integer n, compute the minimum number of operations needed to obtain the number n
 # starting from the number 1.
 
+import sys
+
 def primitive_calculator(n):
     """
 
@@ -12,8 +14,40 @@ def primitive_calculator(n):
     -------
 
     """
+    # theres an alternative to solve this using a while loop checking
+    # on the remainder until it is 0
     k = 1
-    intermediate_results = []
+    intermediate_results = [0] * n
+    remainder = n
+
+    for i in range(1, n):
+        max_move = -1
+        best_move = 0
+
+        if 3 * i > max_move:
+            intermediate_results[i] = 3 * i + intermediate_results[i - 1]
+            max_move = 3 * i
+            best_move = 1
 
 
+        if 2 * i > max_move:
+            intermediate_results[i] = 2 * i + intermediate_results[i - 1]
+            max_move = 2 * i
+            best_move = 1
+
+        if 1 + i > max_move:
+            intermediate_results[i] = 1 + i + intermediate_results[i - 1]
+            max_move = 1 + i
+            best_move = 1
+
+        k += best_move
+        remainder = n - intermediate_results[i]
     return k, intermediate_results
+
+
+if __name__ == '__main__':
+    n = 26
+    print(primitive_calculator(n))
+
+    n = 11
+    print(primitive_calculator(n))
